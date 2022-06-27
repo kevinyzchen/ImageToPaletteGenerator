5d01d4cbc6accc5ea749dd02145acc32fb4b2559
kevinyzchen <54418432+kevinyzchen@users.noreply.github.com>
2022-06-26 23:36:06 -0400

    switching color palette to color space in image processor

--- ImageProcessor.cs
+++ ImageProcessor.cs
@@ -7,14 +7,11 @@ using Godot.Collections;
 
 public class ImageProcessor : FileProcessor, IProcessor
 {
-
     private void ProcessImage(string path, string savePath)
     {
-        GD.PrintErr(savePath);
         if (path.Extension() != "png" && path.Extension() != "jpeg" && path.Extension() != "jpg") return;
         var palette = ImageToPalette(path);
-        GD.Print("got palette", palette.Count);
-        var colorPalette = new ColorPalette(palette, new List<string> { "warm", "fun" });
+        var colorPalette = new ColorSpace(palette, new List<string> { "warm", "fun" }, "random name");
         var result = WritePaletteToDisk(colorPalette, Path.GetFileNameWithoutExtension(path), savePath);
         ResultFilePaths.Add(result);
         FilesProcessed++;
@@ -23,10 +20,10 @@ public class ImageProcessor : FileProcessor, IProcessor
     private List<UberColor> ImageToPalette(string path)
     {
         var pixels = ImageUtility.GetImagePixels(path);
-        var data =  ImageUtility.ColorToData(pixels);
+        var data = ImageUtility.ColorToData(pixels);
         GD.Print(data.Length, "image data");
-        int minK = (int)processingParams["min_k"];
-        int maxK = (int)processingParams["max_k"];
+        var minK = (int)processingParams["min_k"];
+        var maxK = (int)processingParams["max_k"];
         var kMeans = GetKMeans(data, minK, maxK);
         var palette = new List<UberColor>();
         foreach (var i in kMeans.means)
@@ -35,31 +32,25 @@ public class ImageProcessor : FileProcessor, IProcessor
             var newColor = new UberColor(newLab);
             palette.Add(newColor);
         }
+
         return palette;
     }
 
-
-    public List<string> Process(string inputPath, string outputPath, Dictionary args = null)
+    public List<string> Process(string imagePath, string outputPath, Dictionary args = null)
     {
         processingParams = args;
         ResultFilePaths.Clear();
         FilesProcessed = 0;
-        var process = new Process(ProcessImage);
-        ProcessFolder(process, inputPath, outputPath);
+        ProcessFolder(ProcessImage, imagePath, outputPath);
         return ResultFilePaths;
     }
-    
+
     public List<string> Process(List<string> filePaths, string outputPath, Dictionary args = null)
     {
         processingParams = args;
         ResultFilePaths.Clear();
         FilesProcessed = 0;
-        var process = new Process(ProcessImage);
-        foreach (var file in filePaths)
-        {
-            process(file, outputPath);
-        }
+        foreach (var file in filePaths) ProcessImage(file, outputPath);
         return ResultFilePaths;
     }
-    
 }
